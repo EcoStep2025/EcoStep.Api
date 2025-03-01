3d3da5e8f77715e89120cd70234a7334234239ae
JamirGDC <gutierrez.dc@icloud.com>
2025-03-01 18:11:54 +0100

    cambios varios

--- EcoStep.Shared/DTOs/Request/User/UserCreateRequestDto.cs
+++ EcoStep.Shared/DTOs/Request/User/UserCreateRequestDto.cs
@@ -4,7 +4,7 @@ namespace EcoStep.Shared.DTOs.Request.User;
 
 public class UserCreateRequestDto
 {
-    public string FirebaseId { get; set; } = default!;
+    public string? FirebaseId { get; set; } = default!;
     public string? Name { get; set; }
     public string? LastName { get; set; }
     public string? Email { get; set; }
